b974e12df9d61c8c18f451f6d262d850bbcda081
Earl Potters <earl.potters@gmail.com>
2022-10-25 13:04:56 +0200

    near level example

--- client/src/index.js
+++ client/src/index.js
@@ -29,6 +29,7 @@ const wallet = new Wallet({});
 const nonlazy = (component) => lazy(() => component);
 
 const Level = nonlazy(import("./containers/Level"));
+const LevelNear = nonlazy(import("./containers/LevelNear"));
 const Help = nonlazy(import("./containers/Help"));
 const Stats = nonlazy(import("./containers/Stats"));
 
@@ -58,6 +59,7 @@ ReactDOM.render(
               <Switch>
                 <Route path={constants.PATH_HELP} component={Help} />
                 <Route path={constants.PATH_LEVEL} component={Level} />
+                <Route exact path="/near" component={LevelNear} />
                 <Route path={constants.PATH_STATS} component={Stats} />
                 <Route exact path="/" component={App} />
                 <Route path="/" component={NotFound404} />
@@ -107,7 +109,6 @@ window.addEventListener("load", async () => {
     ethutil.attachLogger();
     // @notice: Bunch of Redux actions with middleware to handle
     // Initial web3 related actions
-    debugger;
     store.dispatch(actions.connectWeb3(window.web3));
     // window.web3.eth.getAccounts(function (error, accounts) {
     //   let player;
